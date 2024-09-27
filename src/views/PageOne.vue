b1969df9dd3d45739e91fe93ebdbcc44e66ff166
staka99 <marko.staka12@gmail.com>
2024-09-27 17:21:43 +0200

    Initial commit

---
+++ src/views/PageOne.vue
@@ -0,0 +1,33 @@
+// PageOne.vue
+<template>
+  <div>
+    <h1>{{ uppercasedTitle }}</h1>
+    <input v-model="title" />
+    <button @click="resetTitle">Reset Title</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+
+export default {
+  name: 'PageOne',
+  methods: {
+    resetTitle() {
+      this.title = 'Page One'
+    }
+  },
+  setup() {
+    const title = ref('Page One')
+
+    const uppercasedTitle = computed(() => {
+      return title.value.toUpperCase()
+    })
+
+    return {
+      title,
+      uppercasedTitle
+    }
+  }
+}
+</script>
